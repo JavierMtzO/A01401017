3c5ad4449f220f2946823c942a7a70d184178d47
JavierMtzO <67331708+JavierMtzO@users.noreply.github.com>
2021-05-05 10:53:25 -0500

    README y script de base de datos

---
+++ Examen parcial 2/README.md
@@ -0,0 +1,18 @@
+Segundo examen parcial 
+
+Javier Martínez Olivares
+
+A01401017
+
+Instrucciones
+
+1. Descargar la carpeta "Examen parcial 2"
+2. Abrir en VS Code o en editor de preferencia
+3. Correr su servidor local de mysql ya sea MAMP, XAMP o el que usted use
+4. Agregar una nueva base de datos llamada "examen-parcial2"
+5. Correr el código sql "jurassicPark.sql" dentro de la base de datos en el servidor.
+6. Ajustar password y usuario en el archivo del proyecto: util/database.js
+7. Abrir terminal y ubicarse en la carpeta "Examen parcial 2"
+8. Descargar aplicación con "npm install"
+9. Correr aplicación con el comando "npm start"
+10. Visualizar la aplicacion en el navegador de su preferencia visitando la siguiente URL: http://localhost:3000/
--- Examen parcial 2/jurassicPark.sql
+++ Examen parcial 2/jurassicPark.sql
@@ -1,39 +1,133 @@
-CREATE TABLE `tipos` (
-    `idTipo` int(11) primary key NOT NULL AUTO_INCREMENT,
-    `tipoAccidente` varchar(100) DEFAULT NULL
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 05, 2021 at 03:45 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `examen-parcial2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incidentes`
+--
+
+CREATE TABLE `incidentes` (
+  `idIncidente` int(11) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
+  `lugar` varchar(100) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `incidentes`
+--
+
+INSERT INTO `incidentes` (`idIncidente`, `tipo`, `lugar`, `fecha`) VALUES
+(1, 'Fuga de herbívoro', 'Centro turístico', '2021-05-05 14:14:29'),
+(2, 'Robo de ADN', 'Laboratorios', '2021-05-05 14:14:29'),
+(3, 'Falla eléctrica', 'Restaurante', '2021-05-05 14:14:29'),
+(4, 'Falla eléctrica', 'Centro operativo', '2021-05-05 14:14:29'),
+(6, 'Fuga de Velociraptors', 'Triceratops', '2021-05-05 15:36:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lugares`
+--
+
+CREATE TABLE `lugares` (
+  `idLugar` int(11) NOT NULL,
+  `lugarAccidente` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `lugares`
+--
+
+INSERT INTO `lugares` (`idLugar`, `lugarAccidente`) VALUES
+(2000, 'Centro turístico'),
+(2001, 'Laboratorios'),
+(2002, 'Restaurante'),
+(2003, 'Centro operativo'),
+(2004, 'Triceratops'),
+(2005, 'Dilofosaurios'),
+(2006, 'Velociraptors'),
+(2007, 'TRex'),
+(2008, 'Planicie de los herbívoros');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipos`
+--
+
+CREATE TABLE `tipos` (
+  `idTipo` int(11) NOT NULL,
+  `tipoAccidente` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tipos`
+--
+
+INSERT INTO `tipos` (`idTipo`, `tipoAccidente`) VALUES
+(1000, 'Falla eléctrica'),
+(1001, 'Fuga de herbívoro'),
+(1002, 'Fuga de Velociraptors'),
+(1003, 'Fuga de TRex'),
+(1004, 'Robo de ADN'),
+(1005, 'Auto descompuesto'),
+(1006, 'Visitantes en zona no autorizada');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `incidentes`
+--
+ALTER TABLE `incidentes`
+  ADD PRIMARY KEY (`idIncidente`);
+
+--
+-- Indexes for table `lugares`
+--
+ALTER TABLE `lugares`
+  ADD PRIMARY KEY (`idLugar`);
+
+--
+-- Indexes for table `tipos`
+--
 ALTER TABLE `tipos`
   ADD PRIMARY KEY (`idTipo`);
 
-CREATE TABLE `lugares` (
-    `idLugar` int(11) primary key NOT NULL AUTO_INCREMENT,
-    `lugarAccidente` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`idLugar`);
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
-ALTER TABLE `tipos` AUTO_INCREMENT=1000;
-ALTER TABLE `lugar` AUTO_INCREMENT=2000;
+--
+-- AUTO_INCREMENT for table `incidentes`
+--
+ALTER TABLE `incidentes`
+  MODIFY `idIncidente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- AUTO_INCREMENT for table `lugares`
+--
+ALTER TABLE `lugares`
+  MODIFY `idLugar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2009;
 
-CREATE TABLE `incidentes` (
-    `idIncidente` int(11) primary key NOT NULL AUTO_INCREMENT ,
-    `tipo` varchar(100) DEFAULT NULL,
-    `lugar` varchar(100) DEFAULT NULL,
-    `fecha` timestamp DEFAULT CURRENT_TIME,
-    FOREIGN KEY (idTipo) REFERENCES tipos(idTipo),
-    FOREIGN KEY (idLugar) REFERENCES lugares(idLugar)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `incidentes` ( 
-    `idIncidente` INT(11) primary key NOT NULL AUTO_INCREMENT , 
-    `tipo` VARCHAR(100) NOT NULL , 
-    `lugar` VARCHAR(100) NOT NULL , 
-    `fecha` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `incidentes` (`tipo`, `lugar`) VALUES
-('Fuga de herbívoro','Centro turístico'),
-('Robo de ADN','Laboratorios'),
-('Falla eléctrica','Restaurante'),
-('Falla eléctrica','Centro operativo');
+--
+-- AUTO_INCREMENT for table `tipos`
+--
+ALTER TABLE `tipos`
+  MODIFY `idTipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
